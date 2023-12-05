0647e69fb667a7ec952ba1c068fc46fd82185de4
YangXin0027 <113945690+YangXin0027@users.noreply.github.com>
2023-12-05 11:41:56 -0600

    import abi, setup ethers stuff

--- main.js
+++ main.js
@@ -1,5 +1,116 @@
-let questions = [];
-let openedList = null;
+
+
+main();
+
+async function main() {
+  let questions = [];
+  let openedList = null;
+
+  let contract;
+  let signer;
+  let contractWithSigner;
+  // basic setup code required for all the web pages we make that interact with MetaMask and the Ethereum blockchain
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await provider.send("eth_requestAccounts", []);
+  signer = provider.getSigner();
+  contract = new ethers.Contract(contractAddress, contractABI, provider);
+  contractWithSigner = contract.connect(signer);
+
+  //// ADD YOUR CODE BELOW THIS LINE
+
+  // Function to add a new question to the list
+  function addQuestionToList() {
+    const questionInput = document.querySelector(".question-input");
+    const title = questionInput.value.trim();
+    if (title.length == 0) {
+      return;
+    }
+
+    questions.push(new Question(title, `question-id-${questions.coun}`));
+
+    updateList();
+
+    // Reset question popup
+    questionInput.value = "";
+    document.getElementById("popup").style.display = "none";
+  }
+
+  function updateList() {
+    /* TOP */
+    let rightList = document.getElementById("list-right");
+    rightList.innerHTML = ""; // reset list
+    questions
+      .sort((rhs, lhs) => {
+        if (rhs.answers.length > lhs.answers.length) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+      .forEach((question) => {
+        rightList.prepend(question.createNewListItem());
+      });
+
+    /* NEW */
+    let leftList = document.getElementById("list-left");
+    leftList.innerHTML = ""; // reset list
+    questions
+      .sort((rhs, lhs) => {
+        if (rhs.date > lhs.date) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+      .forEach((question) => {
+        leftList.prepend(question.createNewListItem());
+      });
+  }
+
+  function saveState() {
+    const triangleButtons = document.querySelectorAll(".triangle-button");
+    triangleButtons.forEach((button) => {
+      const answers = button.parentElement.querySelector(".answers");
+      localStorage.setItem(button.parentElement.id, answers.style.display);
+    });
+  }
+
+  function loadState() {
+    const triangleButtons = document.querySelectorAll(".triangle-button");
+    triangleButtons.forEach((button) => {
+      const answers = button.parentElement.querySelector(".answers");
+      const savedState = localStorage.getItem(button.parentElement.id);
+      answers.style.display = savedState === "block" ? "block" : "none";
+      button.parentElement.classList.toggle("open", savedState === "block");
+    });
+  }
+
+  // Add an event listener to the "Mint" button to reset the state
+  var mintButton = document.getElementById("btn");
+  mintButton.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // Load the state from local storage
+    loadState();
+  });
+
+  // Show the popup
+  var btn = document.getElementById("btn");
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("popup").style.display = "block";
+
+    document.getElementById("close-popup").addEventListener("click", function () {
+      document.getElementById("popup").style.display = "none";
+    });
+  });
+
+  document.querySelectorAll(".submit-question").forEach((submitQuestionBtn) => {
+    submitQuestionBtn.removeEventListener("click", addQuestionToList); // Remove the existing click event listener
+    submitQuestionBtn.addEventListener("click", addQuestionToList);
+  });
+
+}
 
 class Answer {
   constructor(text, id) {
@@ -66,7 +177,7 @@ class Question {
 
     const triangleButton = document.createElement("div");
     triangleButton.classList.add("triangle-button", "toggle-list");
-    
+
     triangleButton.addEventListener("click", (event) => {
       event.stopPropagation(); // Prevent the event from bubbling to the parent (item)
 
@@ -117,94 +228,4 @@ class Question {
   };
 }
 
-// Function to add a new question to the list
-function addQuestionToList() {
-  const questionInput = document.querySelector(".question-input");
-  const title = questionInput.value.trim();
-  if (title.length == 0) {
-    return;
-  }
 
-  questions.push(new Question(title, `question-id-${questions.coun}`));
-
-  updateList();
-
-  // Reset question popup
-  questionInput.value = "";
-  document.getElementById("popup").style.display = "none";
-}
-
-function updateList() {
-  /* TOP */
-  let rightList = document.getElementById("list-right");
-  rightList.innerHTML = ""; // reset list
-  questions
-    .sort((rhs, lhs) => {
-      if (rhs.answers.length > lhs.answers.length) {
-        return 1;
-      } else {
-        return -1;
-      }
-    })
-    .forEach((question) => {
-      rightList.prepend(question.createNewListItem());
-    });
-
-  /* NEW */
-  let leftList = document.getElementById("list-left");
-  leftList.innerHTML = ""; // reset list
-  questions
-    .sort((rhs, lhs) => {
-      if (rhs.date > lhs.date) {
-        return 1;
-      } else {
-        return -1;
-      }
-    })
-    .forEach((question) => {
-      leftList.prepend(question.createNewListItem());
-    });
-}
-
-function saveState() {
-  const triangleButtons = document.querySelectorAll(".triangle-button");
-  triangleButtons.forEach((button) => {
-    const answers = button.parentElement.querySelector(".answers");
-    localStorage.setItem(button.parentElement.id, answers.style.display);
-  });
-}
-
-function loadState() {
-  const triangleButtons = document.querySelectorAll(".triangle-button");
-  triangleButtons.forEach((button) => {
-    const answers = button.parentElement.querySelector(".answers");
-    const savedState = localStorage.getItem(button.parentElement.id);
-    answers.style.display = savedState === "block" ? "block" : "none";
-    button.parentElement.classList.toggle("open", savedState === "block");
-  });
-}
-
-// Add an event listener to the "Mint" button to reset the state
-var mintButton = document.getElementById("btn");
-mintButton.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  // Load the state from local storage
-  loadState();
-});
-
-// Show the popup
-var btn = document.getElementById("btn");
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.getElementById("popup").style.display = "block";
-
-  document.getElementById("close-popup").addEventListener("click", function () {
-    document.getElementById("popup").style.display = "none";
-  });
-});
-
-document.querySelectorAll(".submit-question").forEach((submitQuestionBtn) => {
-  submitQuestionBtn.removeEventListener("click", addQuestionToList); // Remove the existing click event listener
-  submitQuestionBtn.addEventListener("click", addQuestionToList);
-});
